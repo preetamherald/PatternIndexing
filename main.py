7d3ad075002ec49bdf521e22dc0d3882b666e040
preetamherald <preetamherald@gmail.com>
2019-11-08 15:41:49 +0530

    used canny for initial crop & removed hardcoded values

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-
 from PIL import ImageGrab, ImageOps, Image, ImageFilter
 import time
 from numpy import *
-import numpy as np
+import cv2
 
 def alignmentCorrecter(image, count):
+    # image = image.transpose(Image.ROTATE_90)
+    # image = image.transpose(Image.ROTATE_90)
+    # return image
     if count>3:
         image.show()
         return image
@@ -15,9 +17,9 @@ def alignmentCorrecter(image, count):
     L = array(imageL.getcolors())
     C = array(imageC.getcolors())
     R = array(imageR.getcolors())
-    # print(L.sum()+R.sum())
-    # print(C.sum())
-    if(((L.sum()+R.sum())>10000)):
+    print(L.sum()+R.sum())
+    print(C.sum())
+    if(((L.sum()+R.sum())>9000)):
         return alignmentCorrecter(image.transpose(Image.ROTATE_90),count+1)
     else:
         image.show()
@@ -45,25 +47,42 @@ def gridValues(image):
 def gridEval(image):
     width, height = image.size
     temp = image.crop((3*width/8, 3*height/8, 5*width/8, 5*height/8))
-    temp.show()
+    # temp.show()
     a = array(temp)
-    return 1 if np.average(a) > 70 else 0
+    return 1 if average(a) > 70 else 0
 
-test_image = "Test4.jpg"
-original = Image.open(test_image)
-original.show()
+def read_N_crop(img):
+    blurred = cv2.blur(img, (3,3))
+    canny = cv2.Canny(blurred, 50, 200)
 
-left = 236
-top = 112
-right = 385
-bottom = 271
-image = original.crop((left, top, right, bottom))
-image = ImageOps.grayscale(image)
+    ## find the non-zero min-max coords of canny
+    pts = argwhere(canny>0)
+    y1,x1 = pts.min(axis=0)
+    y2,x2 = pts.max(axis=0)
 
-alignedImage = alignmentCorrecter(image,0)
-grid = cropGrid(alignedImage)
+    ## crop the region
+    cropped = img[y1:y2, x1:x2]
+    cv2.imwrite("cropped.png", cropped)
 
-grid.show()
-result = gridValues(grid)
+def main():
+    test_image = "Test1 (1).jpg"
+    img = cv2.imread(test_image)
+    read_N_crop(img)
+    original = Image.open("cropped.png")
+    original.show()
+    image = ImageOps.grayscale(original)
 
-print(result)
+    image.show()
+
+    alignedImage = alignmentCorrecter(image,0)
+    grid = cropGrid(alignedImage)
+
+    grid.show()
+    result = gridValues(grid)
+
+    print(result)
+
+if __name__ == "__main__":
+    main()
+else:
+    pass #TODO
